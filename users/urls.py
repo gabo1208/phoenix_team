f609d65ca6cf388ccc41e498a0d1d9813bafde8b
gabo1208 <gabohd@gmail.com>
2017-10-05 00:46:44 -0400

    basic login decorator + .gitignore for pyc files

--- users/urls.py
+++ users/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^logged_index/$', views.index_decorator, name='logged_index'),
 ]
--- users/views.py
+++ users/views.py
@@ -3,6 +3,11 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.http import  HttpResponse
+from django.contrib.auth.decorators import login_required
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Hello, world. You're at index.")
+
+@login_required(login_url='/admin/')
+def index_decorator(request):
+    return HttpResponse("Hello, world. You're at the index, if you're logged in.")
